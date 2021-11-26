01640b6bc262d005b6d92bd22fdd7dd163912cb8
Jaume Ballester <86907672+jaumebt90@users.noreply.github.com>
2021-11-26 12:22:36 +0100

    Update logic.js

--- logic.js
+++ logic.js
@@ -16,6 +16,7 @@ const btnHold = document.querySelector(".btn--hold");
 let scores, currentScore, activePlayer, playing;
 
 //Starting conditions
+
 const init = function () {
   scores = [0, 0];
   currentScore = 0;
